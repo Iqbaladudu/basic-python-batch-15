26b34581ffa386f30408a8c1092bcb48307954d1
Muhammad Iqbal <iqbal.adudu@gmail.com>
2022-04-10 09:15:09 +0200

    basic math operator

---
+++ math_operator.py
@@ -0,0 +1,29 @@
+x: int = 10
+y: int = 2
+z: int = 3
+
+# Operasi Matematika
+
+# 1. Penjumlahan
+penjumlahan: int = x + y + z
+print(f"penjumlahan: {penjumlahan}")
+
+# 2. Pengurangan
+pengurangan: int = x - y - z
+print(f"pengurangan: {pengurangan}")
+
+# 3. Perkalian
+perkalian: int = x * y * z
+print(f"perkalian: {perkalian}")
+
+# 4. Pembagian
+pembagian: int = x / y
+print(f"pembagian: {pembagian}")
+
+# 5. Modulo
+modulo: int = x % z
+print(f"modulo: {modulo}")
+
+# 6. Eksponensial
+eksponensial: int = x ** y
+print(f"eksponensial: {eksponensial}")
