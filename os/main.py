868b4765fcb8af14885b4cf8dca76008d5252a40
Muhammad Iqbal <iqbal.adudu@gmail.com>
2022-04-23 09:29:12 +0200

    done

---
+++ os/main.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+# print(sys.platform)
+# print(os.uname())
+
+print(os.getcwd())
+# change directory
+# os.chdir("../")
+# print(os.getcwd())
+# os.chdir("../")
+# print(os.getcwd())
+
+# os.mkdir("mkdir")
+os.rmdir("mkdir")
